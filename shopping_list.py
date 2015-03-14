4afeaa47122ad269ad875419883f1768219cab80
kevinfeng <kfeng631@gmail.com>
2015-03-14 13:18:22 +0800

    add codefile

---
+++ shopping_list.py
@@ -0,0 +1,18 @@
+shopping_list = list()
+
+print("What should we pick up at the store?")
+print("Enter DONE to stop adding items.")
+
+while True:
+	new_item = input("> ")
+
+	if new_item == "DONE":
+		break
+
+	shopping_list.append(new_item)
+	print("Added! List has {} items.".format(len(shopping_list)))
+
+print("Here's your list:")
+
+for item in shopping_list:
+	print(item)
